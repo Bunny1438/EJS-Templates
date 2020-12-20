7f01c0c02946335c3450c2532067b8e808e6f9d2
Aman Kumar <amansingh45162@gmail.com>
2020-12-20 21:45:27 +0530

    changes

--- app.js
+++ app.js
@@ -12,12 +12,42 @@ app.set('view engine','ejs');
 //Get 
 app.get("/",function (req,res) {
    var today = new Date();
+   var currentDay = today.getDay();
+//    console.log(currentDay)
    var day ="";
-if(today.getDay() === 6 || today.getDay() === 0 ){
-    day="weekend";}
-else{
-    day = "weekday";
-}
+   switch (currentDay) {
+       case 0:
+       day="sunday";
+           
+           break;
+        case 1:
+       day="monday";
+           
+           break;
+        case 2:
+       day="tuesday";
+           
+           break;
+        case 3:
+       day="wdenesday";
+           
+           break;
+        case 4:
+       day="thursday";
+           
+           break;
+        case 5:
+       day="friday";
+           
+           break;
+        case 6:
+       day="saturday";
+           
+           break; 
+       default:
+        day="Error";
+           break;
+   }
 //To render the content on web
 res.render(("list"),{kindOfDay:day})
 });
